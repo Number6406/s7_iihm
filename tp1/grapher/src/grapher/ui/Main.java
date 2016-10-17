a7b2060fa68dbbede4fc10c8cf685f35a1a237b8
Gilles B <bonhoure.gilles@gmail.com>
2016-10-17 14:43:54 +0200

    Spliter

--- tp1/grapher/src/grapher/ui/Main.java
+++ tp1/grapher/src/grapher/ui/Main.java
@@ -2,27 +2,36 @@ package grapher.ui;
 
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
-
+import javax.swing.JList;
+import javax.swing.JSplitPane;
 
 public class Main extends JFrame {
+
+	JList panelList;
+	JSplitPane spliter;
+
 	Main(String title, String[] expressions) {
 		super(title);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
-		Grapher grapher = new Grapher();		
+
+		Grapher grapher = new Grapher();
 		for(String expression : expressions) {
 			grapher.add(expression);
 		}
-		
-		add(grapher);
+
+		panelList = new JList(expressions);
+		spliter = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,panelList,grapher);
+		spliter.setDividerLocation(100);
+		this.add(spliter);
+
 		pack();
 	}
 
 	public static void main(String[] argv) {
 		final String[] expressions = argv;
 		SwingUtilities.invokeLater(new Runnable() {
-			public void run() { 
-				new Main("grapher", expressions).setVisible(true); 
+			public void run() {
+				new Main("grapher", expressions).setVisible(true);
 			}
 		});
 	}
