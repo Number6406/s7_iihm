115ad685fea20c2762fabed083c0ae9a367db48c
Gilles B <bonhoure.gilles@gmail.com>
2016-12-16 22:43:22 +0100

    ajout de la v1 du tp2 pour la conserver

---
+++ tp2/v1/src/downloader/ui/DownloadThread.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package downloader.ui;
+
+import downloader.fc.Downloader;
+
+/**
+ *
+ * @author abonnena & bonhourg
+ */
+public class DownloadThread extends Thread {
+ 
+    Downloader downloader;
+    String filename;
+
+    public DownloadThread(Downloader dl) {
+        this.downloader = dl;
+    }
+    
+    
+    @Override
+    public void run() {
+        try {
+            this.filename = downloader.download();
+        } catch (InterruptedException e) {
+            System.err.println("failed!");
+        }
+    }
+    
+    public String getFileName() {
+        return this.filename;
+    }
+    
+}
---
+++ tp2/v1/src/downloader/ui/Main.java
@@ -0,0 +1,62 @@
+package downloader.ui;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import javax.swing.JFrame;
+import javax.swing.JProgressBar;
+import javax.swing.SwingUtilities;
+
+import downloader.fc.Downloader;
+
+public class Main extends JFrame{
+	
+	JProgressBar prog;
+	
+	public static void main(String argv[]) {
+		final String[] urls = argv;
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new Main("Download", urls);
+			}
+		});
+	}
+
+	Main(String title, String[] urls) {
+		
+		super(title);
+                this.setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+                pack();
+		
+		for(String url : urls){
+			final Downloader dl;
+			try {
+				dl = new Downloader(url);
+			}
+			catch(RuntimeException e) {
+				System.err.format("skipping %s %s\n", url, e);
+				continue;
+			}
+			
+			prog = new JProgressBar(0, 100);
+			prog.setValue(dl.getProgress());
+			prog.setStringPainted(true);
+			this.add(prog);
+                        pack();
+                        
+			dl.addPropertyChangeListener(new  PropertyChangeListener() {
+				public void propertyChange(PropertyChangeEvent evt) {
+                                    System.out.print("." + dl.getProgress());				
+                                    prog.setValue(dl.getProgress());
+				}
+			});
+                        
+                        String filename;
+                        DownloadThread dt = new DownloadThread(dl);
+                        dt.start();
+                        filename = dt.getFileName();
+			System.out.format("Downloaded into %s\n", filename);
+		}
+	}
+}
